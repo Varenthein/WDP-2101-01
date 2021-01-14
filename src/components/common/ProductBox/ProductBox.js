ac6e8ebd9fb448e0047e24ef3aae614c8f680780
kherma <kamil.herma01@gmail.com>
2021-01-14 23:31:22 +0100

    Update colors variables for entire project

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -11,7 +11,6 @@ import {
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
 
-<<<<<<< HEAD
 const ProductBox = ({ name, price, promo, stars, imageSource, oldPrice }) => (
   <div className={styles.root}>
     <div
@@ -20,11 +19,6 @@ const ProductBox = ({ name, price, promo, stars, imageSource, oldPrice }) => (
         backgroundImage: `url("${imageSource}")`,
       }}
     >
-=======
-const ProductBox = ({ name, price, promo, stars }) => (
-  <div className={styles.root}>
-    <div className={styles.photo}>
->>>>>>> bdb3185... deleted unnecessary changes
       {promo && <div className={styles.sale}>{promo}</div>}
       <div className={styles.buttons}>
         <Button variant='small'>Quick View</Button>
@@ -76,10 +70,7 @@ ProductBox.propTypes = {
   oldPrice: PropTypes.number,
   promo: PropTypes.string,
   stars: PropTypes.number,
-<<<<<<< HEAD
   imageSource: PropTypes.string,
-=======
->>>>>>> bdb3185... deleted unnecessary changes
 };
 
 export default ProductBox;
