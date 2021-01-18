5793fee0c8ed5880de350742bc87a0f492c6db83
Mateusz Furman <mateuszfurman.nf@gmail.com>
2021-01-18 22:39:46 +0100

    Add limit to compare & condition for unique choose product

--- src/redux/compareRedux.js
+++ src/redux/compareRedux.js
@@ -15,7 +15,10 @@ export const addToCompare = payload => ({ payload, type: ADD_TO_COMPARE });
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
     case ADD_TO_COMPARE: {
-      return [...statePart, { ...action.payload, id: action.payload.id }];
+      let index = statePart.findIndex(product => product.id === action.payload.id);
+      return statePart.length < 4 && index === -1
+        ? [...statePart, { ...action.payload, id: action.payload.id }]
+        : [...statePart];
     }
     default:
       return statePart;
