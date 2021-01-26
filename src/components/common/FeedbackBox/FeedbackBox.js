fb016133678b3fe70b700f23fade3e1d44da3921
kherma <kamil.herma01@gmail.com>
2021-01-26 20:05:34 +0100

    Add Feedback component layout

--- src/components/common/FeedbackBox/FeedbackBox.js
+++ src/components/common/FeedbackBox/FeedbackBox.js
@@ -1,2 +1,32 @@
 import React from 'react';
 import styles from './FeedbackBox.module.scss';
+
+const FeedbackBox = props => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.box}>
+        <span className={styles.boxQuoteStamp}></span>
+        <p className={styles.boxQuoteText}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae cumque harum
+          soluta voluptate, accusantium iure sunt neque consectetur natus fuga! Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Vitae cumque harum soluta
+          voluptate.
+        </p>
+        <div className={styles.boxUser}>
+          <div className={styles.userImage}>
+            <img
+              src='https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+              alt='user feedback'
+            />
+          </div>
+          <div className={styles.userInfo}>
+            <p className={styles.userName}>John Smith</p>
+            <small>Our Client</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedbackBox;
--- src/components/features/Feedback/Feedback.js
+++ src/components/features/Feedback/Feedback.js
@@ -1,2 +1,47 @@
 import React, { Component } from 'react';
 import styles from './Feedback.module.scss';
+import FeedbackBox from '../../common/FeedbackBox/FeedbackBox';
+
+class Feedback extends Component {
+  state = {
+    currentPage: 0,
+  };
+
+  render() {
+    // TODO changing pages on click, add correct class to active dot
+    const dots = [
+      <li key={1}>
+        <a className={styles.active}>page 1</a>
+      </li>,
+      <li key={2}>
+        <a>page 2</a>
+      </li>,
+      <li key={3}>
+        <a>page 3</a>
+      </li>,
+    ];
+    return (
+      <div className={styles.root}>
+        <div className='container'>
+          <div className={styles.feedback}>
+            <div className='row no-gutters align-items-end'>
+              <div className={'col-auto ' + styles.heading}>
+                <h3>Client feedback</h3>
+              </div>
+              <div className={'col-auto ' + styles.dots}>
+                <ul>{dots}</ul>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <FeedbackBox />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
