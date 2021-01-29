0f53cb1e35b21d3929a252f2b99c1a71fcc4153b
Natalia Szymiczek <nat.szymiczek@gmail.com>
2021-01-29 17:46:26 +0100

    CookiesProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';
 
 import './styles/bootstrap.scss';
 import './styles/global.scss';
@@ -11,18 +13,44 @@ import Homepage from './components/views/Homepage/Homepage';
 import ProductList from './components/views/ProductList/ProductList';
 import ProductPage from './components/views/ProductPage/ProductPage';
 
-const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <MainLayout>
-        <Switch>
-          <Route exact path={'/'} component={Homepage} />
-          <Route exact path={'/shop/:categoryId'} component={ProductList} />
-          <Route exact path={'/product/:productId'} component={ProductPage} />
-        </Switch>
-      </MainLayout>
-    </BrowserRouter>
-  </Provider>
-);
+class App extends Component {
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired,
+  };
 
-export default App;
+  state = {
+    user: this.props.cookies.get('user') || '',
+  };
+
+  handleCookie = () => {
+    const { cookies } = this.props;
+    cookies.set('user', 'gowtham', { path: '/' }); // setting the cookie
+    this.setState({ user: cookies.get('user') });
+  };
+
+  render() {
+    const { user } = this.state;
+    return (
+      <div className="App">
+        <div className="cookieInfo">
+          <h6>Ta strona wykorzystuje pliki cookies.</h6>
+          {user && <p>{user}</p>}
+          <button variant="cookie" onClick={this.handleCookie}>x</button>
+        </div>
+        <Provider store={store}>
+          <BrowserRouter>
+            <MainLayout>
+              <Switch>
+                <Route exact path={'/'} component={Homepage} />
+                <Route exact path={'/shop/:categoryId'} component={ProductList} />
+                <Route exact path={'/product/:productId'} component={ProductPage} />
+              </Switch>
+            </MainLayout>
+          </BrowserRouter>
+        </Provider>
+      </div>
+    );
+  }
+}
+
+export default withCookies(App);
