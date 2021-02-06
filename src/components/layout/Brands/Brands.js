22b03261063df6c4508c8dd29dceefecde764734
Mateusz Furman <mateuszfurman.nf@gmail.com>
2021-02-06 15:51:27 +0100

    Update Brands.js

--- src/components/layout/Brands/Brands.js
+++ src/components/layout/Brands/Brands.js
@@ -5,8 +5,19 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 import Swipeable from '../../common/Swipeable/Swipeable';
 import PropTypes from 'prop-types';
 
+function getBrandsSets({ brands, brandPerSet }) {
+  const setsAmount = Math.ceil(brands / brandPerSet);
+  const sets = [];
+  for (let i = 0; i < setsAmount; i++) {
+    sets.push(brands.slice(i * brandPerSet, i * brandPerSet + brandPerSet));
+  }
+  return sets;
+}
+
 function Brands({ brands }) {
   const [index, setIndex] = useState(0);
+
+  const brandPerSet = 6;
 
   return (
     <div className={styles.root}>
@@ -16,19 +27,23 @@ function Brands({ brands }) {
             <div className={styles.arrow} onClick={() => setIndex(index - 1)}>
               <FontAwesomeIcon className={styles.icon} icon={faChevronLeft} />
             </div>
-            <div className={styles.brandList}>
-              <Swipeable
-                onLeftAction={() => setIndex(index - 1)}
-                onRightAction={() => setIndex(index + 1)}
-                enableMouseEvents
-              >
-                {brands.map(brand => (
-                  <div className={styles.brands} key={brand.id} {...brand}>
-                    <img src={brand.image} alt='brand-logo' />
+            <Swipeable
+              onLeftAction={() => setIndex(index - 1)}
+              onRightAction={() => setIndex(index + 1)}
+              enableMouseEvents
+            >
+              {getBrandsSets(brands, brandPerSet).map(set => {
+                return (
+                  <div className={styles.brandList} key={set.id}>
+                    {set.map(brand => (
+                      <div className={styles.brands} key={brand.id} {...brand}>
+                        <img src={brand.image} alt='brand-logo' />
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </Swipeable>
-            </div>
+                );
+              })}
+            </Swipeable>
             <button className={styles.arrow} onClick={() => setIndex(index + 1)}>
               <FontAwesomeIcon className={styles.icon} icon={faChevronRight} />
             </button>
