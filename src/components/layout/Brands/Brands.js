64f3642cb2fcc0596c2fa23d524df24220900db4
Mateusz Furman <mateuszfurman.nf@gmail.com>
2021-02-07 19:27:23 +0100

    Fix bugs

--- src/components/layout/Brands/Brands.js
+++ src/components/layout/Brands/Brands.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import styles from './Brands.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 import Swipeable from '../../common/Swipeable/Swipeable';
 import PropTypes from 'prop-types';
 
-function getBrandsSets({ brands, brandPerSet }) {
-  const setsAmount = Math.ceil(brands / brandPerSet);
+function getBrandsSets(brands, brandPerSet) {
+  const setsAmount = Math.ceil(brands.length / brandPerSet);
   const sets = [];
   for (let i = 0; i < setsAmount; i++) {
     sets.push(brands.slice(i * brandPerSet, i * brandPerSet + brandPerSet));
@@ -24,9 +23,6 @@ function Brands({ brands }) {
       <div className='container'>
         <div className='row'>
           <div className={styles.wrapper}>
-            <div className={styles.arrow} onClick={() => setIndex(index - 1)}>
-              <FontAwesomeIcon className={styles.icon} icon={faChevronLeft} />
-            </div>
             <Swipeable
               onLeftAction={() => setIndex(index - 1)}
               onRightAction={() => setIndex(index + 1)}
@@ -44,9 +40,6 @@ function Brands({ brands }) {
                 );
               })}
             </Swipeable>
-            <button className={styles.arrow} onClick={() => setIndex(index + 1)}>
-              <FontAwesomeIcon className={styles.icon} icon={faChevronRight} />
-            </button>
           </div>
         </div>
       </div>
